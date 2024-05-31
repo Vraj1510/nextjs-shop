4ab3d9ccbd25bf98835b48c96028883ef75dbd14
Vraj Shah <shahvraj1500@gmail.com>
2024-05-31 13:14:15 +0530

    Final Commit

--- src/hooks/storeHook.ts
+++ src/hooks/storeHook.ts
@@ -1,6 +1,6 @@
-import { RootState } from '@reduxjs/toolkit/query';
-import { AppDispatch } from './../redux/store';
-import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 
-export const useAppDispatch: ()=>AppDispatch=useDispatch;
-export const useAppSelector: TypedUseSelectorHook<RootState> =useSelector;
+import { AppDispatch, RootState } from '@/redux/store';
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
